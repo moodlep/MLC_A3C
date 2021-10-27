e32532b2a2b410e60788adb70d3981a13f73e562
Perusha Moodley <perusha.moodley@gmail.com>
2021-10-27 20:26:58 +0200

    added global t max

--- actor_critic_worker.py
+++ actor_critic_worker.py
@@ -33,65 +33,67 @@ class ActorCriticWorker(mp.Process):
 
     def run(self):
 
-        # 1. Sync local from global - we need this for the actor: get_action()
-        self.actor.load_state_dict(self.global_actor.state_dict())
-        self.critic.load_state_dict(self.global_critic.state_dict())
+        while self.T.value < self.global_t_max:
 
-        # 2. Create a rollout
-        t_start = self.t
-        state   = self.env.reset() #giving us a state from the gym env.
-        done    = False
-        states  = []
-        actions = []
-        rewards = []
-        returns = []
+            # 1. Sync local from global - we need this for the actor: get_action()
+            self.actor.load_state_dict(self.global_actor.state_dict())
+            self.critic.load_state_dict(self.global_critic.state_dict())
 
-        while not done and (self.t - t_start+1)%self.max_step !=0:
-            action = self.actor.get_action(torch.tensor(state, dtype=torch.float).reshape(1,-1))
-            #print(action)
-            next_state, reward,done, _info = self.env.step(action)
-            rewards.append(reward)
-            actions.append(action)
-            states.append(state)
-            state = next_state
-            self.t  += 1
-            # lock memory
-            with self.lock:
-                self.T.value +=1
+            # 2. Create a rollout
+            t_start = self.t
+            state   = self.env.reset() #giving us a state from the gym env.
+            done    = False
+            states  = []
+            actions = []
+            rewards = []
+            returns = []
 
-        # Calculate reward
-        with torch.no_grad():
-            if not done:
-                R = self.critic(torch.tensor(state,dtype = torch.float)).item() #calculating the value function
-            else:
-                R = 0.0
+            while not done and (self.t - t_start+1)%self.max_step !=0:
+                action = self.actor.get_action(torch.tensor(state, dtype=torch.float).reshape(1,-1))
+                #print(action)
+                next_state, reward,done, _info = self.env.step(action)
+                rewards.append(reward)
+                actions.append(action)
+                states.append(state)
+                state = next_state
+                self.t  += 1
+                # lock memory
+                with self.lock:
+                    self.T.value +=1
 
-        for i in range(len(states)-1,-1,-1):  #Reverse because this is a bellman-type of calculation (you know all your rewards from t to the end)
-            R = rewards[i] + self.gamma*R
-            returns.append(R)
-        returns.reverse() # list of returns
+            # Calculate reward
+            with torch.no_grad():
+                if not done:
+                    R = self.critic(torch.tensor(state,dtype = torch.float)).item() #calculating the value function
+                else:
+                    R = 0.0
 
-        # 3. Calculating Loss
-        states_t = torch.tensor(np.array(states), dtype = torch.float)
-        actions_t = torch.tensor(actions, dtype = torch.int)
-        returns_t = torch.tensor(returns, dtype = torch.float)
+            for i in range(len(states)-1,-1,-1):  #Reverse because this is a bellman-type of calculation (you know all your rewards from t to the end)
+                R = rewards[i] + self.gamma*R
+                returns.append(R)
+            returns.reverse() # list of returns
 
-        td_error = returns_t - self.critic(states_t)	# n_batch x 1
-        critic_loss = (td_error)**2 # 1 x 1
-        actor_loss = -1.0*td_error.detach()*self.actor.log_prob(states_t, actions_t) # n_batch x 1
-        # Take mean of the actor and critic loss
-        total_loss = (critic_loss + actor_loss).mean()
+            # 3. Calculating Loss
+            states_t = torch.tensor(np.array(states), dtype = torch.float)
+            actions_t = torch.tensor(actions, dtype = torch.int)
+            returns_t = torch.tensor(returns, dtype = torch.float)
 
-        # 4. Calculate grad and update optimiser
-        self.opt.zero_grad()
-        total_loss.backward()
+            td_error = returns_t - self.critic(states_t)	# n_batch x 1
+            critic_loss = (td_error)**2 # 1 x 1
+            actor_loss = -1.0*td_error.detach()*self.actor.log_prob(states_t, actions_t) # n_batch x 1
+            # Take mean of the actor and critic loss
+            total_loss = (critic_loss + actor_loss).mean()
 
-        # align global grads to local grads
-        for gp, lp in zip(self.global_critic.parameters(), self.critic.parameters()):
-            gp._grad = lp.grad
-        for gp, lp in zip(self.global_actor.parameters(), self.actor.parameters()):
-            gp._grad = lp.grad
+            # 4. Calculate grad and update optimiser
+            self.opt.zero_grad()
+            total_loss.backward()
 
-        # take a step!
-        self.opt.step()
+            # align global grads to local grads
+            for gp, lp in zip(self.global_critic.parameters(), self.critic.parameters()):
+                gp._grad = lp.grad
+            for gp, lp in zip(self.global_actor.parameters(), self.actor.parameters()):
+                gp._grad = lp.grad
+
+            # take a step!
+            self.opt.step()
 
